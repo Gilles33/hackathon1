9515bfecb5363277af57e020f5733795381f2a47
Gilles Martinez <gilles.martinez33@gmail.com>
2017-10-30 19:41:40 +0100

    add html part

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require_once 'weather.php'
 ?>
 
-
 <!DOCTYPE html>
 <html lang="EN">
 <head>
@@ -16,9 +15,25 @@ require_once 'weather.php'
     <label for="city">Enter a city</label>
     <input type="text" placeholder="City..." name="city">
     <input type="submit" value="Search">
-
 </form>
 
+<?php if (!empty($_POST)) { ?>
+    <section id="currentWeather">
+        <h1><?= $cityName ?></h1>
+        <p><?= $today ?></p>
+        <p><img src='http://openweathermap.org/img/w/<?= $icon ?>'/> <?= $weather ?> : <?= $weatherDescription ?></p>
+        <ul>
+            <li>Temperature</li>
+            <ul>
+                <li>Max : <?= $tempMax ?></li>
+                <li>Max : <?= $tempMin ?></li>
+            </ul>
+            <li>Wind : <?= $wind ?> meter/sec</li>
+        </ul>
+    </section>
+    <?php
+}
+?>
 
 </body>
 </html>
